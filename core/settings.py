5df422f799ef6b0291f8e885208aa67ee22c716a
arizonv <a.vasgarridoe@gmail.com>
2023-09-09 14:54:30 -0300

    correccion settings

--- core/settings.py
+++ core/settings.py
@@ -18,6 +18,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+if 'CODESPACE_NAME' in os.environ:
+    codespace_name = os.getenv("CODESPACE_NAME")
+    codespace_domain = os.getenv("GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN")
+    CSRF_TRUSTED_ORIGINS = [f'https://{codespace_name}-8000.{codespace_domain}']
+
 
 DJANGO_APPS = [
     'django.contrib.admin',
@@ -79,8 +84,7 @@ TEMPLATES = [
 
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:4200',
-    'https://localhost:8000'
+    'http://localhost:4200'
 ]
 
 
